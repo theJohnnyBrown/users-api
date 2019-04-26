a7bedfdfedc4084cff327ee82af64c0356958779
Jonathan Brown <jb@zapmaid.com>
2019-04-26 19:04:19 -0400

    loads and parses, sort WIP

---
+++ src/users_api/cli.clj
@@ -0,0 +1,54 @@
+(ns users-api.cli
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :as str]
+            [users-api.data :as data])
+  (:import [java.time.format DateTimeFormatter]))
+
+(def cli-options
+  [["-c" "--commas-file PATH" "Path to a CSV data file"
+    :default []
+    :assoc-fn (fn [m k v]
+                (update-in m [k] conj v))
+    :id :commas-file]
+
+   ["-s" "--spaces-file PATH" "Path to a space-separated data file"
+    :default []
+    :assoc-fn (fn [m k v]
+                (update-in m [k] conj v))
+    :id :spaces-file]
+
+   ["-p" "--pipes-file PATH" "Path to a pipe-separated data file"
+    :default []
+    :assoc-fn (fn [m k v]
+                (update-in m [k] conj v))
+    :id :pipes-file]
+
+   ["-z" "--sort ORDER" "Key by which to sort the results"
+    :parse-fn keyword
+    :default :last-name
+    :validate [#{:dob :gender :last-name}
+               "Please choose one of \"dob\", \"gender\" \"last-name\" "]
+    :id :sort-order]])
+
+(defn user-str [user]
+  (let [u (-> user
+              (update :dob #(.format % (DateTimeFormatter/ofPattern "M/d/YYYY")))
+              (update :gender name))]
+   (str/join ", " (for [k data/user-keys] (get u k)))))
+
+(defn -main [& args]
+  (let [opts (parse-opts args cli-options)]
+    (if-let [e (:errors opts)]
+      (prn e)
+      (let [rows (data/data-seq (concat
+                                 (map #(vector #" *\| *" %)
+                                      (get-in opts [:options :pipes-file]))
+                                 (map #(vector #" +" %)
+                                      (get-in opts [:options :spaces-file]))
+                                 (map #(vector #" *, *" %)
+                                      (get-in opts [:options :commas-file]))))
+
+            sort-fn (get data/sort-orders (get-in opts [:options :sort-order]))
+            _ (prn opts sort-fn)]
+        (doseq [r (sort-fn rows)]
+          (prn (user-str r)))))))
---
+++ src/users_api/data.clj
@@ -0,0 +1,40 @@
+(ns users-api.data
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io])
+  (:import [java.time LocalDate]))
+
+
+(defn lazy-line-seq
+  "Returns a lazy seq of lines from the given reader. Closes the reader
+  once it has been consumed"
+  [rdr]
+  (lazy-seq (if-let [next (.readLine rdr)]
+              (cons next (lazy-line-seq rdr))
+              (.close rdr))))
+
+(def user-keys [:last-name :first-name :gender :favorite-color :dob])
+
+(defn row->rowmap
+  "converts a string into a map of keywords and parsed values"
+  [separator row]
+  (let [keys user-keys
+        vals (str/split row separator)]
+    (-> (zipmap keys vals)
+        (update :gender #(condp contains? (str/lower-case %)
+                           #{"f" "female" "woman"} :female
+                           #{"m" "male" "man"}     :male
+                           :other))
+        (update :dob #(LocalDate/parse (str/trim %))))))
+
+(defn path->rows [separator filename]
+  (->> filename io/reader lazy-line-seq
+       (map (partial row->rowmap separator))))
+
+(defn data-seq
+  "`files` should be a seq of vectors that look like [separator, path]"
+  [files]
+  (mapcat #(apply path->rows %) files))
+
+(def sort-orders {:gender (partial sort-by (juxt :gender (comp :last-name str/lower-case)))
+                  :last-name   #(reverse (sort-by (comp :last-name str/lower-case) %))
+                  :dob    (partial sort-by :dob)})
